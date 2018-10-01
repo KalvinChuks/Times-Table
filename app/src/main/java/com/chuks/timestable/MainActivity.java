e5450dad784b4e50562eb4803080a9cebbbca2e9
Kalvin Chuks <chuks.kalvin@gmail.com>
2018-10-01 11:43:50 +0000

    Added both SeekBar and ListView

--- app/src/main/java/com/chuks/timestable/MainActivity.java
+++ app/src/main/java/com/chuks/timestable/MainActivity.java
@@ -2,12 +2,22 @@ package com.chuks.timestable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends Activity {
+
+    private ListView listView;
+    private SeekBar seekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        
     }
 }
